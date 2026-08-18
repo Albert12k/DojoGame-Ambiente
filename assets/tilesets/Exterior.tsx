9435bd5a5733a118021175b6cb0af5d0a4aded33
Albert <albertsantososares01@gmail.com>
2026-08-18 15:51:45 -0300

    feat: cria tilemap inicial do labirinto

---
+++ assets/tilesets/Exterior.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="Exterior" tilewidth="32" tileheight="32" tilecount="320" columns="40">
+ <image source="OuterTileset.png" width="1280" height="256"/>
+</tileset>
